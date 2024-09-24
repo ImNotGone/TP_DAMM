e52a3094699e7cb7628ff4855d0666fd2bed8985
Gonzalo A. Martone <gmartone@itba.edu.ar>
2024-09-24 11:55:31 -0300

    feat: Volunteering text

--- lib/main.dart
+++ lib/main.dart
@@ -61,52 +61,66 @@ class Home extends StatelessWidget {
             Tab(icon: Text(AppLocalizations.of(context)!.news)),
           ]),
         ),
-        // TODO: remove this.
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(0.0),
-                  border: Border.all(color: Colors.grey[300]!),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.2),
-                      spreadRadius: 1,
-                      blurRadius: 5,
-                      offset: const Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: Row(
-                  children: [
-                    const Icon(Icons.search, color: Colors.grey),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: (AppLocalizations.of(context)!.search),
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.map, color: Colors.green),
-                      onPressed: () {
-                        // TODO: agregar accion
-                      },
-                    ),
-                  ],
-                ),
-              )
+              _SearchBar(),
+              const Padding(padding: EdgeInsets.all(8.0)),
+              Text(
+                AppLocalizations.of(context)!.volunteering,
+                style: const TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 24.0)
+              ),
             ],
           ),
         )
       ),
     );
   }
+}
+
+class _SearchBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(0.0),
+        border: Border.all(color: Colors.grey[300]!),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 1,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          const Icon(Icons.search, color: Colors.grey),
+          const SizedBox(width: 10),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: (AppLocalizations.of(context)!.search),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.map, color: Colors.green),
+            onPressed: () {
+              // TODO: agregar accion
+            },
+          ),
+        ],
+      ),
+    );
+  }
+  
 }
