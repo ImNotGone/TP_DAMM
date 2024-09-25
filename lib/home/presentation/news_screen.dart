d65ebeca9ce1991020fd9d51fa4d7323e9c9b98e
Gonzalo A. Martone <gmartone@itba.edu.ar>
2024-09-25 19:14:33 -0300

    feat: use riverpod as provider for news

--- lib/home/presentation/news_screen.dart
+++ lib/home/presentation/news_screen.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
-import '../../constants.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ser_manos_mobile/providers/news_provider.dart';
+import '../domain/news.dart';
 import 'news_card.dart';
 
-class NewsScreen extends StatelessWidget {
+class NewsScreen extends ConsumerWidget {
   const NewsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<News> allNews = ref.watch(newsProvider);
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
           Expanded(
-              child: ListView(
-                children: [
-                  NewsCard(news: news1),
-                  NewsCard(news: news2),
-                  NewsCard(news: news3),
-                  NewsCard(news: news4)
-                ],
+              child: ListView.builder(
+                itemCount: allNews.length,
+                itemBuilder: (context, index) {
+                  return NewsCard(news: allNews[index]);
+                },
               )
           ),
         ],
---
+++ lib/providers/news_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'news_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$newsHash() => r'3d1bea83451035d8b8964661d72595d5624052da';
+
+/// See also [news].
+@ProviderFor(news)
+final newsProvider = AutoDisposeProvider<List<News>>.internal(
+  news,
+  name: r'newsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$newsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef NewsRef = AutoDisposeProviderRef<List<News>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
