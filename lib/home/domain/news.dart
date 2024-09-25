9e3db06b0e83a37c25bcce829d9567bc11378956
Gonzalo A. Martone <gmartone@itba.edu.ar>
2024-09-25 13:24:14 -0300

    feat: News model class

---
+++ lib/home/domain/news.dart
@@ -0,0 +1,15 @@
+class News {
+  String imageUrl;
+  String title;
+  String paper;
+  String subtitle;
+  String content;
+
+  News({
+    required this.imageUrl,
+    required this.title,
+    required this.paper,
+    required this.subtitle,
+    required this.content
+  });
+}
