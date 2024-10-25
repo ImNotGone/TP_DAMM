b135b8efd18853426af5f05c328878a70ab9cd8a
Franco Bosetti <francobosetti@live.com.ar>
2024-10-24 21:01:47 -0300

    chore: login uses auth service provider
    also, the login button stops being grayed out when one character is written in password controller and viceversa

--- lib/auth/presentation/login.dart
+++ lib/auth/presentation/login.dart
@@ -2,97 +2,114 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ser_manos_mobile/auth/application/auth_service.dart';
 import 'package:ser_manos_mobile/auth/presentation/postlogin_welcome.dart';
 import 'package:ser_manos_mobile/auth/presentation/signup.dart';
+import 'package:ser_manos_mobile/providers/auth_service_provider.dart';
 import 'package:ser_manos_mobile/shared/molecules/buttons/filled.dart';
 import '../../shared/molecules/buttons/text.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends HookConsumerWidget {
   const LoginScreen({super.key});
 
   @override
-  LoginScreenState createState() => LoginScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final emailController = useTextEditingController();
+    final passwordController = useTextEditingController();
+    final obscurePassword = useState(true);
+    final isButtonEnabled = useState(false);
 
-class LoginScreenState extends State<LoginScreen> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  bool _obscurePassword = true;
+    final AuthService authServiceInstance = ref.watch(authServiceProvider);
 
-  final AuthService _authService = AuthService();
+    // Enable or disable the login button when text changes
+    void updateButtonState() {
+      isButtonEnabled.value = emailController.text.isNotEmpty &&
+          passwordController.text.isNotEmpty;
+    }
 
-  @override
-  Widget build(BuildContext context) {
+    // Attach listeners to the text controllers
+    useEffect(() {
+      emailController.addListener(updateButtonState);
+      passwordController.addListener(updateButtonState);
+
+      // Clean up listeners when widget is disposed
+      return () {
+        emailController.removeListener(updateButtonState);
+        passwordController.removeListener(updateButtonState);
+      };
+    }, []);
+
+    Future<void> handleLogin() async {
+      final user = await authServiceInstance.signInWithEmailAndPassword(
+        emailController.text,
+        passwordController.text,
+      );
+
+      if (user != null && context.mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const PostLoginWelcome()),
+        );
+      } else {
+        log('Login failed');
+        // Handle login failure (e.g., show a snackbar or dialog)
+      }
+    }
+
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Spacer(),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Image.asset('assets/logo_square.png'),
-                const SizedBox(height: 16),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        hintText: AppLocalizations.of(context)!.email,
-                        border: const OutlineInputBorder(),
-                      ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/logo_square.png'), // Logo at the top
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: _buildTextField(
+                      controller: emailController,
+                      hintText: AppLocalizations.of(context)!.email,
                       keyboardType: TextInputType.emailAddress,
-                      onChanged: (value) => setState(() {}),
                     ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                        hintText: AppLocalizations.of(context)!.password,
-                        border: const OutlineInputBorder(),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _obscurePassword
-                                ? Icons.visibility
-                                : Icons.visibility_off,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _obscurePassword = !_obscurePassword;
-                            });
-                          },
-                        ),
-                      ),
-                      obscureText: _obscurePassword,
-                      onChanged: (value) => setState(() {}),
+                  ),
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: _buildPasswordField(
+                      passwordController: passwordController,
+                      obscurePassword: obscurePassword,
+                      hintText: AppLocalizations.of(context)!.password,
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
-            const Spacer(),
-            Column(
-              children: [
-                UtilFilledButton(
-                    onPressed: (_emailController.text.isNotEmpty &&
-                        _passwordController.text.isNotEmpty)
-                        ? _handleLogin
-                        : null,
-                    text: AppLocalizations.of(context)!.login),
-                const SizedBox(height: 8),
-                UtilTextButton(
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  UtilFilledButton(
+                    onPressed: isButtonEnabled.value ? handleLogin : null,
+                    text: AppLocalizations.of(context)!.login,
+                  ),
+                  const SizedBox(height: 8),
+                  UtilTextButton(
                     onPressed: () {
                       Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const SignUpScreen()));
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SignUpScreen()),
+                      );
                     },
-                    text: AppLocalizations.of(context)!.noAccount),
-              ],
+                    text: AppLocalizations.of(context)!.noAccount,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -100,26 +117,44 @@ class LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Future<void> _handleLogin() async {
-    final user = await _authService.signInWithEmailAndPassword(
-      _emailController.text,
-      _passwordController.text,
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String hintText,
+    TextInputType keyboardType = TextInputType.text,
+  }) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+        border: const OutlineInputBorder(),
+      ),
+      keyboardType: keyboardType,
     );
-
-    if (user != null) {
-      _navigateToWelcome();
-    } else {
-      log('Login failed');
-      // Handle login failure (e.g., show a snackbar or dialog)
-    }
   }
 
-  void _navigateToWelcome() {
-    if (mounted) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const PostLoginWelcome()),
-      );
-    }
+  Widget _buildPasswordField({
+    required TextEditingController passwordController,
+    required ValueNotifier<bool> obscurePassword,
+    required String hintText,
+  }) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: obscurePassword,
+      builder: (context, obscure, _) {
+        return TextField(
+          controller: passwordController,
+          obscureText: obscure,
+          decoration: InputDecoration(
+            hintText: hintText,
+            border: const OutlineInputBorder(),
+            suffixIcon: IconButton(
+              icon: Icon(
+                obscure ? Icons.visibility : Icons.visibility_off,
+              ),
+              onPressed: () => obscurePassword.value = !obscurePassword.value,
+            ),
+          ),
+        );
+      },
+    );
   }
 }
