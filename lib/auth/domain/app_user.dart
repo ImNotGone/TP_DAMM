2a027a216baed5e89c5581f7ff2688623d0bf40f
Franco Bosetti <francobosetti@live.com.ar>
2024-10-28 21:24:57 -0300

    chore: add copywith method to app user

--- lib/auth/domain/app_user.dart
+++ lib/auth/domain/app_user.dart
@@ -20,6 +20,10 @@ class AppUser {
     required this.firstName,
     required this.lastName,
     required this.email,
+    this.birthDate,
+    this.gender,
+    this.phoneNumber,
+    this.profilePictureURL
   });
 
   bool isComplete (){
@@ -30,6 +34,29 @@ class AppUser {
   factory AppUser.fromJson(Map<String, dynamic> json) => _$AppUserFromJson(json);
 
   Map<String, dynamic> toJson() => _$AppUserToJson(this);
+
+  AppUser copyWith({
+    String? uid,
+    String? firstName,
+    String? lastName,
+    String? email,
+    DateTime? birthDate,
+    Gender? gender,
+    String? phoneNumber,
+    String? profilePictureURL,
+  }) {
+    return AppUser(
+      uid: uid ?? this.uid,
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      email: email ?? this.email,
+      birthDate: birthDate ?? this.birthDate,
+      gender: gender ?? this.gender,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      profilePictureURL: profilePictureURL ?? this.profilePictureURL,
+    );
+  }
+
 }
 
 enum Gender{
