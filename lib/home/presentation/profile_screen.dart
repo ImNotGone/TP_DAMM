a273eff49fdd9913fa0711abb6d80ce4c8d7ade5
Franco Bosetti <francobosetti@live.com.ar>
2024-10-30 17:48:32 -0300

    fix: profile picture in circle

--- lib/home/presentation/profile_screen.dart
+++ lib/home/presentation/profile_screen.dart
@@ -39,85 +39,81 @@ class _MissingDataScreen extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Center(child: Column(
+    return Center(
+        child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-      CircleAvatar(
-      radius: 50,
-      backgroundColor: Colors.blue[100],
-      child: currentUser.profilePictureURL == null
-          ? Icon(
-        Icons.person,
-        size: 50,
-        color: Theme
-            .of(context)
-            .colorScheme
-            .primary,
-      )
-          : Image.network(
-        currentUser.profilePictureURL!,
-        fit: BoxFit.cover,
-      ),
-      ),
-      const SizedBox(height: 20),
-      Text(
-        AppLocalizations.of(context)!.volunteer.toUpperCase(),
-        style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2,
+        CircleAvatar(
+          radius: 50,
+          backgroundColor: Colors.blue[100],
+          child: currentUser.profilePictureURL == null
+              ? Icon(
+                  Icons.person,
+                  size: 50,
+                  color: Theme.of(context).colorScheme.primary,
+                )
+              : ClipOval(
+                  child: Image.network(
+                    currentUser.profilePictureURL!,
+                    fit: BoxFit.cover,
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
         ),
-      ),
-      const SizedBox(height: 10),
-      Text(
-        currentUser.firstName,
-        style: const TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      const SizedBox(height: 10),
-      Text(
-        AppLocalizations.of(context)!.completeProfile,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.grey[700],
-        ),
-      ),
-      const SizedBox(height: 80),
-      ElevatedButton.icon(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return const ProfileEditScreen();
-          }));
-        },
-        icon: const Icon(Icons.add),
-        label: Text(AppLocalizations.of(context)!.complete),
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Theme
-              .of(context)
-              .colorScheme
-              .secondary,
-          foregroundColor: Theme
-              .of(context)
-              .colorScheme
-              .onSecondary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-      ),
-      TextButton(
-        onPressed: () {
-          // Add your logout logic here
-        },
-        child: Text(
-          AppLocalizations.of(context)!.signOut,
+        const SizedBox(height: 20),
+        Text(
+          AppLocalizations.of(context)!.volunteer.toUpperCase(),
           style: const TextStyle(
-            color: Colors.red,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
           ),
         ),
-      ),
+        const SizedBox(height: 10),
+        Text(
+          currentUser.firstName,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 10),
+        Text(
+          AppLocalizations.of(context)!.completeProfile,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.grey[700],
+          ),
+        ),
+        const SizedBox(height: 80),
+        ElevatedButton.icon(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return const ProfileEditScreen();
+            }));
+          },
+          icon: const Icon(Icons.add),
+          label: Text(AppLocalizations.of(context)!.complete),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Theme.of(context).colorScheme.secondary,
+            foregroundColor: Theme.of(context).colorScheme.onSecondary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            // Add your logout logic here
+          },
+          child: Text(
+            AppLocalizations.of(context)!.signOut,
+            style: const TextStyle(
+              color: Colors.red,
+            ),
+          ),
+        ),
       ],
     ));
   }
