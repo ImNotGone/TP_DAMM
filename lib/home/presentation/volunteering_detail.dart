04b0e2771505e549ff856516ad663224970e3aed
Gonzalo A. Martone <gmartone@itba.edu.ar>
2024-10-31 15:54:56 -0300

    ref: better follow figma

--- lib/home/presentation/volunteering_detail.dart
+++ lib/home/presentation/volunteering_detail.dart
@@ -3,6 +3,7 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ser_manos_mobile/home/presentation/info_card.dart';
 import 'package:ser_manos_mobile/providers/volunteering_provider.dart';
 import 'package:ser_manos_mobile/shared/molecules/components/vacancies.dart';
 import '../../shared/molecules/buttons/filled.dart';
@@ -39,105 +40,114 @@ class VolunteeringDetail extends HookConsumerWidget {
   }
 
   Widget _buildContent(BuildContext context, Volunteering volunteering) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Image
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: Image.network(
-              volunteering.imageUrl,
-              fit: BoxFit.cover,
-            ),
-          ),
-          const SizedBox(height: 16),
-
-          // Title and Type
-          Text(
-            volunteering.type.localizedName(context),
-            style: Theme.of(context).textTheme.labelMedium,
-          ),
-          const SizedBox(height: 8),
-          Text(
-            volunteering.title,
-            style: Theme.of(context).textTheme.headlineSmall,
-          ),
-          const SizedBox(height: 16),
-
-          // Description
-          Text(
-            volunteering.purpose,
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-          const SizedBox(height: 16),
-
-          // Activity Details
-          Text(
-            AppLocalizations.of(context)!.activityDetailsTitle,
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          const SizedBox(height: 8),
-          Text(
-            volunteering.activityDetail,
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-          const SizedBox(height: 16),
-
-          // Location
-          Text(
-            AppLocalizations.of(context)!.location,
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          const SizedBox(height: 8),
-          _buildLocation(context, volunteering),
-
-          const SizedBox(height: 16),
-
-          // Participation Info
-          _buildParticipationInfo(context, volunteering),
-
-          // Apply Button
-          const SizedBox(height: 16),
-          UtilFilledButton(
-            onPressed: () {
-              // Handle apply logic
-            },
-            text: AppLocalizations.of(context)!.applyForVolunteering,
-          )
-        ],
-      ),
-    );
-  }
-
-  // TODO: this should be in the shared card with blue header!!
-  Widget _buildLocation(BuildContext context, Volunteering volunteering) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Container(
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            color: const Color(0xFFCAE5FB),
-            borderRadius: BorderRadius.circular(8),
+        // Image at the top
+        ClipRRect(
+          child: Image.network(
+            volunteering.imageUrl,
+            fit: BoxFit.cover,
+            width: double.infinity, // Make it full width
+            height: 200.0, // Set a fixed height for the image
           ),
-          child: Center(
-            child: Text(
-              AppLocalizations.of(context)!.location,
-              style: const TextStyle(fontSize: 16),
+        ),
+        const SizedBox(height: 16), // Space between image and content
+
+        // Main content area with a white background
+        Expanded(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              color: Colors.white, // Set the background color to white
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Title and Type
+                  Text(
+                    volunteering.type.localizedName(context).toUpperCase(),
+                    style: Theme.of(context).textTheme.labelSmall,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    volunteering.title,
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                  const SizedBox(height: 16),
+
+                  // Description
+                  Text(
+                    volunteering.purpose,
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: const Color(0xFF0D47A1)),
+                  ),
+                  const SizedBox(height: 16),
+
+                  // Activity Details
+                  Text(
+                    AppLocalizations.of(context)!.activityDetailsTitle,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    volunteering.activityDetail,
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  const SizedBox(height: 16),
+
+                  _buildLocation(context, volunteering),
+
+                  const SizedBox(height: 16),
+
+                  // Participation Info
+                  _buildParticipationInfo(context, volunteering),
+
+                  // Apply Button
+                  const SizedBox(height: 16),
+                  UtilFilledButton(
+                    onPressed: () {
+                      // Handle apply logic
+                    },
+                    text: AppLocalizations.of(context)!.applyForVolunteering,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-        const SizedBox(height: 8),
-        // TODO: insert map, now dummy
-        Image.network(
-          'https://staticmapmaker.com/img/google-placeholder.png',
-          fit: BoxFit.cover,
-        ),
-        const SizedBox(height: 8),
-        Text(volunteering.address),
       ],
+    );
+  }
+
+  Widget _buildLocation(BuildContext context, Volunteering volunteering) {
+    return CustomCard(
+      title: AppLocalizations.of(context)!.location,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // TODO: insert map, now dummy
+          Image.network(
+            'https://staticmapmaker.com/img/google-placeholder.png',
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 8),
+              Text(
+                AppLocalizations.of(context)!.address.toUpperCase(),
+                style: Theme.of(context).textTheme.labelSmall
+              ),
+              const SizedBox(height: 4),
+              Text(
+                volunteering.address,
+                style: Theme.of(context).textTheme.bodyLarge
+              ),
+              const SizedBox(height: 8),
+            ],
+          )
+        ],
+      ),
     );
   }
 
@@ -151,7 +161,7 @@ class VolunteeringDetail extends HookConsumerWidget {
       children: [
         Text(
           AppLocalizations.of(context)!.participateInVolunteering,
-          style: Theme.of(context).textTheme.titleMedium,
+          style: Theme.of(context).textTheme.headlineMedium,
         ),
         const SizedBox(height: 8),
 
