c3ee725d2f5c35b7f0a20549e9a381b17c6243d9
Gonzalo A. Martone <gmartone@itba.edu.ar>
2024-11-05 19:16:36 -0300

    fix: properly align volunteering card

--- lib/home/presentation/volunteering_card.dart
+++ lib/home/presentation/volunteering_card.dart
@@ -29,57 +29,71 @@ class VolunteeringCard extends StatelessWidget {
             ClipRRect(
               child: Image.network(
                 volunteering.imageUrl,
-                height: 150,
+                height: 138,
                 width: double.infinity,
                 fit: BoxFit.cover,
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Text(
-                    volunteering.type.localizedName(context),
+                    volunteering.type.localizedName(context).toUpperCase(),
                     style: Theme.of(context).textTheme.labelSmall,
                   ),
-                  const SizedBox(height: 4),
                   // Title Text
                   Text(
                     volunteering.title,
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
                   // Vacancy Row
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Vacancies(count: volunteering.vacancies),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          IconButton(
-                            icon: const Icon(Icons.favorite_border),
-                            color: Colors.green,
-                            onPressed: () {
+                  const SizedBox(height: 4),
+                  SizedBox(
+                    height: 28,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Vacancies(count: volunteering.vacancies),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            _buildIcon(Icons.favorite_border, () {
                               // TODO: Handle favorite button press
-                            },
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.place),
-                            color: Colors.green,
-                            onPressed: () {
+                            }),
+                            const SizedBox(width: 16,),
+                            _buildIcon(Icons.place, () {
                               // TODO: Handle location button press
-                            },
-                          ),
-                        ],
-                      ),
-                    ],
+                            }),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
+                  const SizedBox(height: 8,)
                 ],
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildIcon(
+      IconData icon,
+      VoidCallback? onPressed
+      ) {
+    return SizedBox(
+      width: 24,
+      height: 24,
+      child: IconButton(
+        icon: Icon(icon,),
+        color: Colors.green,
+        onPressed: onPressed,
+        padding: EdgeInsets.zero,
+        constraints: const BoxConstraints(),
       ),
     );
   }
