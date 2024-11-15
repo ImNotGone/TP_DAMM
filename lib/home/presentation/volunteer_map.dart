385769073a7541f6794af9664236bed72dc123d3
Gonzalo A. Martone <gmartone@itba.edu.ar>
2024-11-14 22:46:12 -0300

    feat: add currentLocation to map

--- lib/home/presentation/volunteer_map.dart
+++ lib/home/presentation/volunteer_map.dart
@@ -1,8 +1,10 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ser_manos_mobile/providers/location_provider.dart';
 import 'package:ser_manos_mobile/shared/molecules/buttons/floating.dart';
 import '../../providers/volunteering_provider.dart';
 import '../../shared/cells/cards/volunteer_card.dart';
@@ -27,6 +29,8 @@ class VolunteerMapScreen extends HookConsumerWidget {
     final notSelectedMarker = useState(BitmapDescriptor.defaultMarker);
 
     final allVolunteerings = ref.watch(volunteeringsNotifierProvider);
+    final location = useState<LatLng?>(null);
+    final currentPosition = useState<Position?>(null);
 
     final filteredVolunteerings = allVolunteerings?.values
         .where((volunteering) => volunteering.title
@@ -60,68 +64,102 @@ class VolunteerMapScreen extends HookConsumerWidget {
       );
     }
 
-    return Stack(
-      children: [
-        GoogleMap(
-          initialCameraPosition: const CameraPosition(
-            target: LatLng(-34.563, -58.443), // Center on your preferred location
-            zoom: 14.0,
-          ),
-          onMapCreated: (controller) {
-            // Save controller for further interactions if needed
-            mapController.value = controller;
-          },
-          zoomControlsEnabled: false,
-          myLocationButtonEnabled: false,
-          myLocationEnabled: true,
-          markers: markers.value,
-        ),
-        Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              // pongo el padding aca pq las tarjetas se extienden hasta el borde de la pantalla -> no tienen padding horizontal
-              Padding(
-                padding: const EdgeInsets.fromLTRB(16, 24, 16, 0),
-                child: UtilSearchBar(
-                    onIconPressed: onIconPressed,
-                    onSearchChanged: (query) {
-                      searchQuery.value = query;
-                    },
-                    icon: Icons.list,
-                ),
+    Future<void> getUserLocation(ValueNotifier<LatLng?> center, ValueNotifier<Position?> currentPosition) async {
+      bool serviceEnabled;
+      LocationPermission permission;
+
+      serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        return;
+      }
+
+      permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.deniedForever) {
+        return;
+      }
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission != LocationPermission.whileInUse && permission != LocationPermission.always) {
+          return;
+        }
+      }
+
+      currentPosition.value = await Geolocator.getCurrentPosition();
+      final latLng = LatLng(currentPosition.value!.latitude, currentPosition.value!.longitude);
+      location.value = latLng;
+      ref.read(locationNotifierProvider.notifier).setLocation(latLng);
+    }
+
+    useEffect(() {
+      getUserLocation(location, currentPosition);
+      return null;
+    }, []);
+
+
+    return location.value == null
+        ? const Center(child: CircularProgressIndicator())
+        : Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: location.value!, // Center on your preferred location
+                zoom: 14.0,
               ),
-              Column(
+              onMapCreated: (controller) {
+                mapController.value = controller;
+              },
+              zoomControlsEnabled: false,
+              myLocationButtonEnabled: false,
+              myLocationEnabled: true,
+              markers: markers.value,
+            ),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
+                  // pongo el padding aca pq las tarjetas se extienden hasta el borde de la pantalla -> no tienen padding horizontal
                   Padding(
-                    padding: const EdgeInsets.only(right: 16),
-                    child: UtilFloatingButton(onPressed: () => {
-                        // TODO: posicion actual del usuario
-                      },
+                    padding: const EdgeInsets.fromLTRB(16, 24, 16, 0),
+                    child: UtilSearchBar(
+                        onIconPressed: onIconPressed,
+                        onSearchChanged: (query) {
+                          searchQuery.value = query;
+                        },
+                        icon: Icons.list,
                     ),
                   ),
-                  const SizedBox(height: 16,),
-                  Padding(
-                      padding: const EdgeInsets.only(bottom: 16),
-                      child: filteredVolunteerings?.isEmpty ?? true
-                          ? const NoVolunteering()
-                          : CarouselSlider(
-                        items: filteredVolunteerings!.map((volunteering) =>
-                            VolunteerCard(volunteeringId: volunteering.uid)).toList(),
-                        options: CarouselOptions(
-                            height: 242,
-                            viewportFraction: 0.9,
-                            enableInfiniteScroll: false
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 16),
+                        child: UtilFloatingButton(onPressed: () => {
+                            mapController.value?.animateCamera(
+                              CameraUpdate.newLatLng(location.value!)
+                            )
+                          },
                         ),
                       ),
+                      const SizedBox(height: 16,),
+                      Padding(
+                          padding: const EdgeInsets.only(bottom: 16),
+                          child: filteredVolunteerings?.isEmpty ?? true
+                              ? const NoVolunteering()
+                              : CarouselSlider(
+                            items: filteredVolunteerings!.map((volunteering) =>
+                                VolunteerCard(volunteeringId: volunteering.uid)).toList(),
+                            options: CarouselOptions(
+                                height: 242,
+                                viewportFraction: 0.9,
+                                enableInfiniteScroll: false
+                            ),
+                          ),
+                      )
+                    ],
                   )
                 ],
               )
-
-            ],
-          )
-      ],
-    );
+          ],
+        );
   }
 }
