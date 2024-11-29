90a19f2931e5ae98ff8afface8394eefa9909589
Gonzalo A. Martone <gmartone@itba.edu.ar>
2024-11-29 14:57:33 -0300

    fix: no volunteerings is now shown when it should

--- lib/volunteer/presentation/volunteer_screen.dart
+++ lib/volunteer/presentation/volunteer_screen.dart
@@ -1,9 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ser_manos_mobile/volunteer/presentation/volunteer_list.dart';
 import 'package:ser_manos_mobile/volunteer/presentation/volunteer_map.dart';
-import 'package:ser_manos_mobile/providers/user_provider.dart';
 import 'package:ser_manos_mobile/providers/volunteering_provider.dart';
 
 import '../../providers/service_providers.dart';
@@ -15,13 +16,11 @@ class VolunteerScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final showMap = useState(false);
+    final isLoading = useState(true);
 
     void toggleMap() {
       showMap.value = !showMap.value;
     }
-
-    final allVolunteerings = ref.watch(volunteeringsNotifierProvider);
-    final currentUser = ref.watch(currentUserNotifierProvider);
 
     final volunteeringsNotifier = ref.read(volunteeringsNotifierProvider.notifier);
     final volunteeringService = ref.read(volunteeringServiceProvider);
@@ -29,6 +28,7 @@ class VolunteerScreen extends HookConsumerWidget {
     Future<void> loadVolunteerings() async {
       await for (var volunteerings in volunteeringService.fetchVolunteerings()) {
         volunteeringsNotifier.setVolunteerings(volunteerings);
+        isLoading.value = false;
       }
     }
 
@@ -37,18 +37,14 @@ class VolunteerScreen extends HookConsumerWidget {
       return null;
     }, []);
 
-    if (currentUser == null ||
-        allVolunteerings == null ||
-        allVolunteerings.isEmpty) {
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
-
-    return showMap.value
-        ? VolunteerMapScreen(onIconPressed: toggleMap)
-        : VolunteerListScreen(onIconPressed: toggleMap);
+    return isLoading.value
+            ? Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                )
+              )
+            : showMap.value
+              ? VolunteerMapScreen(onIconPressed: toggleMap)
+              : VolunteerListScreen(onIconPressed: toggleMap);
   }
 }
