3875be3b2bdefab6ef5d510495a4ea9ba6ae35b4
Gonzalo A. Martone <gmartone@itba.edu.ar>
2024-12-17 16:35:28 -0300

    fix: now news cards can be pressed to go to detail

--- lib/shared/cells/cards/news_card.dart
+++ lib/shared/cells/cards/news_card.dart
@@ -17,59 +17,64 @@ class NewsCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(2),
-          color: SerManosColors.neutral0,
-          boxShadow: SerManosShadows.shadow2
-      ),
-      child: IntrinsicHeight(
-        child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SizedBox(
-                width: 118,
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(2),
-                    bottomLeft: Radius.circular(2),
+    return GestureDetector(
+        onTap: () {
+          context.push('/newsDetail/${news.uid}');
+        },
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(2),
+              color: SerManosColors.neutral0,
+              boxShadow: SerManosShadows.shadow2
+          ),
+          child: IntrinsicHeight(
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(
+                    width: 118,
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(2),
+                        bottomLeft: Radius.circular(2),
+                      ),
+                      child: Image.network(
+                        news.imageUrl,
+                        fit: BoxFit.cover,
+                        // somehow making the image smaller, makes it take the lowest available space
+                        height: double.minPositive,
+                      ),
+                    ),
                   ),
-                  child: Image.network(
-                    news.imageUrl,
-                    fit: BoxFit.cover,
-                    // somehow making the image smaller, makes it take the lowest available space
-                    height: double.minPositive,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(padding: const EdgeInsets.only(top: 16.0, left: 8.0, right: 8.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(news.paper, style: SerManosTextStyle.overline()),
-                      Text(news.title, style: SerManosTextStyle.subtitle01()),
-                      Text(news.subtitle, style: SerManosTextStyle.body02().copyWith(color: SerManosColors.neutral75)),
-                      Align(
-                          alignment: Alignment.bottomRight,
-                          child: TextButton(
-                              onPressed: () {
-                                context.push('/newsDetail/${news.uid}');
-                              },
-                              child: Text(
-                                AppLocalizations.of(context)!.readMore,
-                                style: SerManosTextStyle.button().copyWith(color: SerManosColors.primary100),
+                  Expanded(
+                    child: Padding(padding: const EdgeInsets.only(top: 16.0, left: 8.0, right: 8.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(news.paper, style: SerManosTextStyle.overline()),
+                          Text(news.title, style: SerManosTextStyle.subtitle01()),
+                          Text(news.subtitle, style: SerManosTextStyle.body02().copyWith(color: SerManosColors.neutral75)),
+                          Align(
+                              alignment: Alignment.bottomRight,
+                              child: TextButton(
+                                  onPressed: () {
+                                    context.push('/newsDetail/${news.uid}');
+                                  },
+                                  child: Text(
+                                    AppLocalizations.of(context)!.readMore,
+                                    style: SerManosTextStyle.button().copyWith(color: SerManosColors.primary100),
+                                  )
                               )
                           )
-                      )
-                    ],
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ]
-        ),
-      ),
-    );
+                ]
+            ),
+          ),
+        )
+      );
   }
 }
