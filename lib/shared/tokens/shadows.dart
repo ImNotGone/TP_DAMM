15b406eac460f4f480fd7e25cedb146da2d180f4
Gonzalo A. Martone <gmartone@itba.edu.ar>
2024-12-18 16:50:37 -0300

    ref: change deprecated api

--- lib/shared/tokens/shadows.dart
+++ lib/shared/tokens/shadows.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 abstract final class SerManosShadows {
   static List<BoxShadow> shadow1 = [
     BoxShadow(
-      color: Colors.black.withOpacity(0.15),
+      color: Colors.black.withValues(alpha: 0.15),
       offset: const Offset(0, 1),
       blurRadius: 3,
       spreadRadius: 1,
     ),
     BoxShadow(
-      color: Colors.black.withOpacity(0.30),
+      color: Colors.black.withValues(alpha: 0.30),
       offset: const Offset(0, 1),
       blurRadius: 2,
       spreadRadius: 0,
@@ -17,13 +17,13 @@ abstract final class SerManosShadows {
   ];
   static List<BoxShadow> shadow2 = [
       BoxShadow(
-        color: Colors.black.withOpacity(0.15),
+        color: Colors.black.withValues(alpha: 0.15),
         offset: const Offset(0, 2),
         blurRadius: 6,
         spreadRadius: 2,
       ),
       BoxShadow(
-        color: Colors.black.withOpacity(0.30),
+        color: Colors.black.withValues(alpha: 0.30),
         offset: const Offset(0, 1),
         blurRadius: 2,
         spreadRadius: 0,
@@ -31,13 +31,13 @@ abstract final class SerManosShadows {
   ];
   static List<BoxShadow> shadow3 = [
     BoxShadow(
-      color: Colors.black.withOpacity(0.15),
+      color: Colors.black.withValues(alpha: 0.15),
       offset: const Offset(0, 8),
       blurRadius: 12,
       spreadRadius: 6,
     ),
     BoxShadow(
-      color: Colors.black.withOpacity(0.30),
+      color: Colors.black.withValues(alpha: 0.30),
       offset: const Offset(0, 4),
       blurRadius: 4,
       spreadRadius: 0,
